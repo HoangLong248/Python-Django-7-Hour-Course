7dab5cdf95be06ef04d5e363b44998479fbaa017
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-11-13 19:32:02 +0700

    finished edit user account page section

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .form import RoomForm
+from .form import RoomForm, UserForm
 
 
 
@@ -162,3 +162,15 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='/login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    context = {'form': form}
+    return render(request, 'base/update_user.html', context)
